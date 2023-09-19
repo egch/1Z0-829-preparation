59bfdf098478d8176aeb3b4a6f7d48f2ad822d67
Enrico Giurin <enricogiurin@gmail.com>
2023-09-19 03:24:19 +0200

    ch10: reviewing streams

---
+++ src/main/java/org/enricogiurin/ocp17/ch10/CollectingResults.java
@@ -0,0 +1,62 @@
+package org.enricogiurin.ocp17.ch10;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CollectingResults {
+
+  public static void main(String[] args) {
+    new CollectingResults().groupingBySet();
+  }
+
+  void averagingInt() {
+    Stream<String> fruitStream = fruitStream();
+    Double average = fruitStream.collect(Collectors.averagingInt(value -> value.length()));
+    System.out.println("average: "+average); //4.9
+  }
+
+  void map() {
+    Stream<String> fruitStream = fruitStream();
+    Map<Integer, String> map = fruitStream.collect(
+        Collectors.toMap(String::length, f -> f, (f1, f2) -> f1 + ", " + f2));
+    System.out.println(map);  //{4=Kiwi, Pear, Lime, 5=Apple, Grape, Peach, Mango, Lemon, 6=Banana, Orange}
+    System.out.println(map.getClass());  //class java.util.TreeMap
+  }
+
+  void treeMap() {
+    Stream<String> fruitStream = fruitStream();
+    TreeMap<Integer, String> map = fruitStream.collect(
+        Collectors.toMap(String::length,
+            f -> f,
+            (f1, f2) -> f1 + ", " + f2,
+            TreeMap::new));
+
+    System.out.println(map);  //{4=Kiwi, Pear, Lime, 5=Apple, Grape, Peach, Mango, Lemon, 6=Banana, Orange}
+    System.out.println(map.getClass());  //class java.util.HashMap
+  }
+
+  void groupingBy() {
+    Stream<String> fruitStream = fruitStream();
+    Map<Integer, List<String>> map = fruitStream.collect(Collectors.groupingBy(String::length));
+    System.out.println(map);  //{4=[Kiwi, Pear, Lime], 5=[Apple, Grape, Peach, Mango, Lemon], 6=[Orange, Banana]}
+  }
+
+  void groupingBySet() {
+    Stream<String> fruitStream = fruitStream();
+    Map<Integer, Set<String>> map = fruitStream.collect(Collectors.groupingBy(String::length,
+        Collectors.toSet()));
+    System.out.println(map);  //{4=[Lime, Kiwi, Pear], 5=[Apple, Grape, Peach, Mango, Lemon], 6=[Orange, Banana]}
+  }
+
+  private Stream<String> fruitStream() {
+    return Stream.of("Orange", "Apple", "Banana", "Grape", "Kiwi",
+        "Pear", "Peach", "Mango", "Lemon", "Lime");
+  }
+
+
+}
---
+++ src/main/java/org/enricogiurin/ocp17/ch10/primitivestream/UsageOfLongStream.java
@@ -0,0 +1,36 @@
+package org.enricogiurin.ocp17.ch10.primitivestream;
+
+import java.util.OptionalDouble;
+import java.util.stream.LongStream;
+
+public class UsageOfLongStream {
+
+  public static void main(String[] args) {
+    new UsageOfLongStream().average();
+  }
+
+  void sum() {
+    LongStream range = LongStream.range(1, 20);
+    long sum = range.sum();
+    System.out.println("sum is: "+sum);
+  }
+
+  void average() {
+    LongStream rangeClosed = LongStream.rangeClosed(1, 20);
+    //note that is an OptionalDouble
+    OptionalDouble optionalDouble = rangeClosed.average();
+    if(optionalDouble.isPresent()){
+      System.out.println("average is: "+optionalDouble.getAsDouble()); //average is: 10.5
+    }
+
+    LongStream emptyStream = LongStream.empty();
+    optionalDouble = emptyStream.average();
+    if(optionalDouble.isPresent()){
+      System.out.println("average is: "+optionalDouble.getAsDouble());
+    }else{
+      System.out.println("no data found");
+    }
+
+  }
+
+}
