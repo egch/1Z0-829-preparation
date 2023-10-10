9df4601ccf7c5c94b181039f28fba7f17b35ae9c
Enrico Giurin <enricogiurin@gmail.com>
2023-10-11 01:05:18 +0200

    ch15: reviewed Stored Procedures

--- src/main/java/org/enricogiurin/ocp17/ch15/UseOfPreparedStatement.java
+++ src/main/java/org/enricogiurin/ocp17/ch15/UseOfPreparedStatement.java
@@ -15,27 +15,28 @@ public class UseOfPreparedStatement {
     UseOfPreparedStatement instance = new UseOfPreparedStatement();
     instance.insert();
     instance.update_execute();
-    instance.select();
+    instance.selectWithBind();
     instance.delete();
   }
 
   void insert() throws SQLException {
-    try(Connection connection = DriverManager.getConnection(JDBC_URL);
+    try (Connection connection = DriverManager.getConnection(JDBC_URL);
         PreparedStatement preparedStatement = connection.prepareStatement(
             "insert into games (id, name) values (1, ?)")) {
       preparedStatement.setString(1, "m-football");
       int row = preparedStatement.executeUpdate();
-      System.out.println("inserted: "+row);
+      System.out.println("inserted: " + row);
     }
   }
+
   void update() throws SQLException {
-    try(Connection connection = DriverManager.getConnection(JDBC_URL);
-      PreparedStatement preparedStatement = connection.prepareStatement(
-          "update games set description = ? where name = ?")) {
+    try (Connection connection = DriverManager.getConnection(JDBC_URL);
+        PreparedStatement preparedStatement = connection.prepareStatement(
+            "update games set description = ? where name = ?")) {
       preparedStatement.setString(1, "football five as side");
       preparedStatement.setString(2, "m-football");
       int row = preparedStatement.executeUpdate();
-      System.out.println("updated: "+row);
+      System.out.println("updated: " + row);
     }
   }
 
@@ -50,32 +51,49 @@ public class UseOfPreparedStatement {
                 " - name: " + resultSet.getString("name")
                 + " - description: " + resultSet.getString("description"));
       }
-
     }
+  }
+
+  void selectWithBind() throws SQLException {
+    try (Connection connection = DriverManager.getConnection(JDBC_URL);
+        PreparedStatement preparedStatement = connection.prepareStatement(
+            "select id, name, description from games where id != ?")) {
+
+      //I cannot set the ps here within the try-with-resources
+      preparedStatement.setInt(1, 0);
+      try (ResultSet resultSet = preparedStatement.executeQuery()) {
+        while (resultSet.next()) {
+          System.out.println(
+              "id: " + resultSet.getInt("id") +
+                  " - name: " + resultSet.getString("name")
+                  + " - description: " + resultSet.getString("description"));
+        }
+      }//inner try-with-resources
+    }  //outer try-with-resources
   }
 
 
   void update_execute() throws SQLException {
-    try(Connection connection = DriverManager.getConnection(JDBC_URL);
+    try (Connection connection = DriverManager.getConnection(JDBC_URL);
         PreparedStatement preparedStatement = connection.prepareStatement(
             "update games set description = ? where name = ?")) {
       preparedStatement.setString(1, "football five as side");
       preparedStatement.setString(2, "m-football");
       //here we use execute()
       boolean isResultSet = preparedStatement.execute();
-      if(!isResultSet) {
-        System.out.println("updated: "+preparedStatement.getUpdateCount());
+      if (!isResultSet) {
+        System.out.println("updated: " + preparedStatement.getUpdateCount());
       }
     }
   }
 
   void delete() throws SQLException {
-    try(Connection connection = DriverManager.getConnection(JDBC_URL);
+    try (Connection connection = DriverManager.getConnection(JDBC_URL);
         PreparedStatement preparedStatement = connection.prepareStatement(
             "delete from games where name=?")) {
       preparedStatement.setString(1, "m-football");
       int row = preparedStatement.executeUpdate();
-      System.out.println("deleted: "+row);
+      System.out.println("deleted: " + row);
     }
   }
 
