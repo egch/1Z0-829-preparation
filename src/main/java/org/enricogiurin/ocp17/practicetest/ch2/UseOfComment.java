629a0d0cfe263d9b8a32168bb4a73fb1b950d2c1
Enrico Giurin <enricogiurin@gmail.com>
2023-11-20 13:17:56 +0100

    tricky comments

---
+++ src/main/java/org/enricogiurin/ocp17/practicetest/ch2/UseOfComment.java
@@ -0,0 +1,17 @@
+package org.enricogiurin.ocp17.practicetest.ch2;
+
+public class UseOfComment {
+
+    public static void main(String[] args) {
+        new UseOfComment().fromSOF();
+    }
+
+    void fromSOF() {
+        // \u000a System.out.println("execute");
+// \u0009 System.out.println("execute");
+// \n System.out.println("execute");
+// \u000d System.out.println("execute");
+
+
+    }
+}
