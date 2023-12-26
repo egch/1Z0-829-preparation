db7253ca77900b470f1964a54155684d2e1a586e
Enrico Giurin <enricogiurin@gmail.com>
2023-12-26 02:34:01 +0100

    practice test - reviewing ch 4 (handling exceptions)

---
+++ src/main/java/org/enricogiurin/ocp17/book/ch11/exceptions/ThrowInCatch.java
@@ -0,0 +1,27 @@
+package org.enricogiurin.ocp17.book.ch11.exceptions;
+
+public class ThrowInCatch {
+
+  public static void main(String[] args) {
+    new ThrowInCatch().handleCatch();
+  }
+
+
+  //it prints WXZ followed by the stacktrace
+
+  void handleCatch() {
+    try {
+      System.out.print('W');
+      throw new ArrayIndexOutOfBoundsException();
+    } catch (RuntimeException r) {
+      System.out.print('X');
+      //this is not caught in the next catch(Exception e) block
+      throw r;
+    } catch (Exception e) {
+      System.out.print('Y');
+    } finally {
+      System.out.print('Z');
+    }
+  }
+
+}
---
+++ src/main/java/org/enricogiurin/ocp17/book/ch11/trywithresources/OutOfScope.java
@@ -0,0 +1,16 @@
+package org.enricogiurin.ocp17.book.ch11.trywithresources;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class OutOfScope {
+  void tryWithResource() {
+    try(Scanner sc = new Scanner(System.in)) {
+      String s = sc.nextLine();
+    }catch (Exception e) {
+      //sc at this point is out of scope!
+      //System.out.println(sc); does not compile!
+    }
+  }
+
+}
