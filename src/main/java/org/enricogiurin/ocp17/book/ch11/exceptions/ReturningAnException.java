ea92b7c2e08615986e4b706fe52e75c4534ac1a0
Enrico Giurin <enricogiurin@gmail.com>
2023-12-26 22:43:50 +0100

    removed a wrong assumption over exception

--- src/main/java/org/enricogiurin/ocp17/book/ch11/exceptions/ReturningAnException.java
+++
@@ -1,9 +0,0 @@
-package org.enricogiurin.ocp17.book.ch11.exceptions;
-
-public class ReturningAnException {
-  //I couldn't believe but this does compile!
-  Exception handle(RuntimeException e) {
-    throw e;
-  }
-
-}
