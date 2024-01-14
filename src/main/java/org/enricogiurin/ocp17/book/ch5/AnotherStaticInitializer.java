2563a2879e5c6a28b45b5a366681dda948d19f61
Enrico Giurin <enricogiurin@gmail.com>
2024-01-15 00:30:51 +0100

    static initializer

---
+++ src/main/java/org/enricogiurin/ocp17/book/ch5/AnotherStaticInitializer.java
@@ -0,0 +1,19 @@
+package org.enricogiurin.ocp17.book.ch5;
+
+import org.enricogiurin.ocp17.various.helper.Enthuware;
+
+@Enthuware
+public class AnotherStaticInitializer {
+  public static void main(String[] args){
+    //10
+    System.out.println(Sub.ID);
+  }
+}
+
+class Super { static String ID = "10"; }
+
+class Sub extends Super{
+  static { System.out.print("static Sub");
+  }
+}
+
